585f52da6478cfe7e4d7afd55dde6c92db679d9b
Tia <ts1035@student.aru.ac.uk>
2025-12-03 17:21:39 +0000

    Added migration for ThresholdSettings and then updated database for it

---
+++ SensoreApp/Migrations/20251203172004_AddThresholdSettingsTable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SensoreApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddThresholdSettingsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ThresholdSettings",
+                columns: table => new
+                {
+                    ThresholdID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserID = table.Column<int>(type: "int", nullable: true),
+                    ThresholdValue = table.Column<decimal>(type: "decimal(5,2)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ThresholdSettings", x => x.ThresholdID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ThresholdSettings");
+        }
+    }
+}
