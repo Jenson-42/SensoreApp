4430fa8992157399507bca330bd182a49a275f03
Tia <ts1035@student.aru.ac.uk>
2025-12-10 05:12:02 +0000

    Added logging to sensor device controller before creating controller actions. Will then add logging statements into the SensorDeviceController actions

---
+++ SensoreApp/Controllers/SensorDevicesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc; // Using directive for ASP.NET Core MVC as instructed to for app structure
+using Microsoft.EntityFrameworkCore; // Using directive for Entity Framework Core for database operations
+using SensoreApp.Data; // Using directive for the application's data context
+using SensoreApp.Models; // Using directive for the application's models
+
+namespace SensoreApp.Controllers
+{
+    public class SensorDevicesController : Controller
+    {
+        private readonly AppDBContext _context; 
+        private readonly ILogger<SensorDevicesController> _logger; // to add logging to system for easier debugging and tracking as well as auditing for admin dashboard
+
+        // Dependency injection of the database context and logger
+        public SensorDevicesController(AppDBContext context, ILogger<SensorDevicesController> logger)
+        {
+            _context = context;
+            _logger = logger; // to initialise the logger implementation
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
