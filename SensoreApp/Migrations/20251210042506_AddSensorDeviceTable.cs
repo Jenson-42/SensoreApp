06ad86caf4cca27c9f369af4b0a60e396849e9fb
Tia <ts1035@student.aru.ac.uk>
2025-12-10 04:31:00 +0000

    -Implemented sensor device model with the proper PK and relevant constrainsts for the EF -Cleaned up migrating issues in package manager console, by rebuilding local database then created new migration.

---
+++ SensoreApp/Migrations/20251210042506_AddSensorDeviceTable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SensoreApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSensorDeviceTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SensorDevices",
+                columns: table => new
+                {
+                    SensorDeviceID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SerialNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Model = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    RegisteredAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SensorDevices", x => x.SensorDeviceID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SensorDevices");
+        }
+    }
+}
