c61bef4709caeaa50f41fec4404f451280fa9fd8
Tia <ts1035@student.aru.ac.uk>
2025-12-10 17:55:12 +0000

    Revert "Fixed SensorDevices Model - Added an additional property called IsActive"
    This reverts commit 74c2e28f3d078d2d98522ae03e5f8a4d21fe20ff.

--- SensoreApp/Migrations/AppDBContextModelSnapshot.cs
+++ SensoreApp/Migrations/AppDBContextModelSnapshot.cs
@@ -226,9 +226,6 @@ namespace SensoreApp.Migrations
 
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("SensorDeviceID"));
 
-                    b.Property<bool>("IsActive")
-                        .HasColumnType("bit");
-
                     b.Property<string>("Model")
                         .IsRequired()
                         .HasMaxLength(100)
