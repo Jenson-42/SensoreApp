ce1b0716e4e63c253bffc295e667f2b4cb9cf5fe
Tia <ts1035@student.aru.ac.uk>
2025-12-13 04:59:54 +0000

    Added a UserRole enum for managing Patients/ Clinicians/ Admins in admin dashboard

---
+++ SensoreApp/Models/UserRole.cs
@@ -0,0 +1,12 @@
+﻿namespace SensoreApp.Models
+{
+    // Enum to represent define user roles in the system
+    // This is to prevent invalid values being used for user roles
+    public enum UserRole
+    // will be stored as a number in the database
+    {
+        Patient,
+        Clinician,
+        Admin
+    }
+}
