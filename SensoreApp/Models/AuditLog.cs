f36826a4cb4f5ef07cd3b0bdab97a7805d2ac127
Tia <ts1035@student.aru.ac.uk>
2025-12-15 03:26:41 +0000

    Created Audit Log table

---
+++ SensoreApp/Models/AuditLog.cs
@@ -0,0 +1,13 @@
+﻿namespace SensoreApp.Models
+{
+    public class AuditLog
+    {
+        // additional model created to log important actions for
+        // auditing purposes (AssigningClinician, CreatingUser, DeactivateUser, EditUser etc)
+        public int AuditLogID { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string Action { get; set; } = string.Empty; // e.g., "CREATE", "UPDATE", "DELETE"    
+        public int PerformedByUserID { get; set; } 
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
